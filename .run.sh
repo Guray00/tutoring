212ce088aec2896dd6b7e80fbbaf79bdab183c0f
Marco <>
2021-05-02 19:59:06 +0000

    Aggiunto supporto python e C

--- .run.sh
+++ .run.sh
@@ -1,9 +1,22 @@
 
 # =============================================
-			MAIN="esAutoMoto"
-			FOLDER="AutoMoto"
-			LANGUAGE="Java"
+			MAIN="cicli"
+			LOCATION="Fondamenti"
+			LANGUAGE="Python"
 # =============================================
 
+# JAVA CASE
+if [ $LANGUAGE = "Java" ]
+then
+	cd /home/runner/tutoring/$LANGUAGE/$LOCATION && javac ./$MAIN.java && java $MAIN
 
-cd /home/runner/tutoring/$LANGUAGE/Esercizi/$FOLDER/ && javac ./$MAIN.java && java $MAIN
+# PYTHON CASE
+elif [ $LANGUAGE = "Python" ]
+then
+	cd /home/runner/tutoring/$LANGUAGE/$LOCATION && python3 ./$MAIN.py
+
+# C CASE
+elif [ $LANGUAGE = "C" ]
+then
+	cd /home/runner/tutoring/$LANGUAGE/$LOCATION && gcc $MAIN.c -o $MAIN && ./$MAIN && rm $MAIN
+fi
---
+++ Python/Fondamenti/cicli.py
@@ -0,0 +1,12 @@
+
+# esempio sul funzionamento dei cicli
+
+print("Esempio di for:")
+for i in range(10):
+	print(i)
+
+print("\n\bEsempio di while")
+i = 3
+while ( i >= 0):
+	print(i)
+	i-=1
