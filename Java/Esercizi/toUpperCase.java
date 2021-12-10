396bfc8e02209020e22634be5941dde518a19af0
Marco Lampis <30523962+Guray00@users.noreply.github.com>
2021-12-10 17:25:27 +0000

    es java

---
+++ Java/Esercizi/toUpperCase.java
@@ -0,0 +1,18 @@
+public class toUpperCase{
+
+	public static void main(String[] args){
+    
+		char v[] = {'C', 'I', 'a', 'O'};
+
+
+        for (int i = 0; i<v.length; i++){
+            v[i] = Character.toUpperCase(v[i]);
+            System.out.println(v[i]);
+        }
+
+
+
+
+
+	}
+}
