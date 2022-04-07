3c1941c732e05ad256d2792088e3f3e0bf864305
Marco Lampis <30523962+Guray00@users.noreply.github.com>
2022-04-07 17:07:48 +0000

    cambiato file java

--- Calcola area/calcolaArea.java
+++ Calcola area/calcolaArea.java
@@ -1,17 +1,58 @@
-import java.util.*;
-import java.lang.Math;
+import java.io.*;  
 
-public class JavaApplication {
-    private static Scanner input = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        int base, altezza, area;
-        
-        System.out.println("Inserisci la base");
-        base = input.nextInt();
-        System.out.println("Inserisci l'altezza");
-        altezza = input.nextInt();
-        area = base * altezza;
-        System.out.println("L'area del rettangolo vale " + area);
+public class es{
+
+	public static void main(String[] args){
+
+    // stiamo creando un "oggetto" che ci consenta di 
+    // chiedere all'utente di inserire dei dati
+    // (stiamo spiegando al java come chiedere in input)
+    BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+    // prova a fare questo....
+    try {
+      // dichiarazione delle variabili => creiamo le variabili
+  		int base, altezza, area; 
+  
+      // Chiedere di inserire la base
+      System.out.print("Inserisci la base: ");
+  
+      // dobbiamo inserire la base facendo input
+      //input.readLine() => legge in input fino a che non premiamo invio
+      // readLine() => ti legge una stringa, quindi una frase
+      // ma a me serve un numero!!!! (quindi un int)
+
+      // leggo a schermo quello che scrive l'utente
+      String str_appo;
+      str_appo = input.readLine();
+
+      // converto la stringa str_appo in un intero
+      base = Integer.parseInt(str_appo);
+
+      // chiediamo di inserire l'altezza
+      System.out.print("Inserisci l'altezza: ");
+
+      // inseriamo l'altezza come stringa (frase)
+      str_appo = input.readLine();
+
+      // dobbiamo convertire di nuovo l'altezza in un numero
+      altezza = Integer.parseInt(str_appo);
+
+      // faccio il calcolo
+      area = base*altezza;
+
+      // mostriamo a schermo il risultato
+      System.out.print("L'area risultante è "+ area);
+      
+   }
+
+    // se sbaglio, errore
+    catch (Exception exc){
+      System.out.println("errore...");
     }
+  
+      
+	  
+  }
 }
