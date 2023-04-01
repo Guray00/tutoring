675bf3f92992baf88e1ef373f1843e45bbf36941
Marco Lampis <30523962+Guray00@users.noreply.github.com>
2023-04-01 17:54:39 +0000

    caricato esercizio

---
+++ pari e dispari doppio e triplo/es.py
@@ -0,0 +1,8 @@
+# chiediamo all'utente di inserire il numero
+numero = int(input("Inserisci un numero: "))
+
+if numero%2 == 0:
+  print(numero*2)
+
+else:
+  print(numero*3)
--- replit.nix
+++ replit.nix
@@ -1,7 +1,8 @@
 { pkgs }: {
 	deps = [
-		pkgs.nano
-  pkgs.jq.bin
+		pkgs.python310
+    pkgs.nano
+    pkgs.jq.bin
     pkgs.clang_12
 		pkgs.ccls
 		pkgs.gdb
